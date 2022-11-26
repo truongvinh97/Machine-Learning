38b6b5422532e97d1725c6a63abf6232350e9b19
Vinh Nguyen <vinhdeka@gmail.com>
2022-11-27 03:57:18 +0700

    Create Assignment 2 Inclass
    Assigment 2 inclass
    
    Signed-off-by: Vinh Nguyen <vinhdeka@gmail.com>

---
+++ NguyenTruongVinh_Assignment2_inclass-HK221/README.txt
@@ -0,0 +1,4 @@
+# This is the result of Assignment 2 inclass
+==================================================
+My github link: https://github.com/truongvinh97/Machine-Learning/tree/master/NguyenTruongVinh_Assignment2_inclass-HK221
+==================================================
---
+++ NguyenTruongVinh_Assignment2_inclass-HK221/dnn_tf.py
@@ -0,0 +1,187 @@
+import numpy as np
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+tf.enable_eager_execution()
+tf.keras.backend.clear_session()
+import matplotlib.pyplot as plt
+from util import * 
+from dnn_np import test
+from sklearn.metrics import confusion_matrix
+import pdb
+
+
+def vehicle_classification():
+    # Load data from file
+    # Make sure that bat.dat is in data/
+    train_x, train_y, test_x, test_y = get_vehicle_data()
+    train_x, test_x = normalize_vehicle(train_x, test_x)    
+
+    test_y  = test_y.flatten().astype(np.int32)
+    train_y = train_y.flatten().astype(np.int32)
+    num_class = (np.unique(train_y)).shape[0]
+
+    train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1]*train_x.shape[2]))
+    test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1]*test_x.shape[2]))
+ 
+    # DNN parameters
+    hidden_layers = [100, 100, 100]
+    learning_rate = 0.01
+    batch_size = 200
+    steps = 500
+   
+    # Specify that all features have real-value data
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[train_x.shape[1]])]
+
+
+    # Available activition functions
+    # https://www.tensorflow.org/versions/r0.12/api_docs/python/nn/activation_functions_
+    # tf.nn.relu
+    # tf.nn.elu
+    # tf.nn.sigmoid
+    # tf.nn.tanh
+    activation = tf.nn.relu
+    
+    # [TODO 1.7] Create a neural network and train it using estimator
+
+    # Some available gradient descent optimization algorithms
+    # https://www.tensorflow.org/api_guides/python/train#Optimizers
+    # tf.train.GradientDescentOptimizer
+    # tf.train.AdadeltaOptimizer
+    # tf.train.AdagradOptimizer
+    # tf.train.AdagradDAOptimizer
+    # tf.train.MomentumOptimizer
+    # tf.train.AdamOptimizer
+    # tf.train.FtrlOptimizer
+    # tf.train.ProximalGradientDescentOptimizer
+    # tf.train.ProximalAdagradOptimizer
+    # tf.train.RMSPropOptimizer
+    # Create optimizer
+    optimizer = tf.train.ProximalAdagradOptimizer(
+                                    learning_rate=learning_rate, 
+                                    l2_regularization_strength=0.001)
+
+    # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+    # optimizer = tf.train.MomentumOptimizer(learning_rate=0.01, momentum=0.005)
+    
+    # build a deep neural network
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/DNNClassifier
+    classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns, 
+                                    hidden_units= hidden_layers,
+                                    n_classes=num_class,
+                                    optimizer= optimizer,
+                                    activation_fn= activation,
+                                    model_dir="problem1-estimator")
+    
+    # Define the training inputs
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/inputs/numpy_input_fn
+    train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+                                    x={"x": train_x},
+                                    y=train_y,
+                                    num_epochs=None, # None will run forever.
+                                    batch_size = batch_size,
+                                    shuffle=True)
+    
+    # Train model.
+    classifier.train(
+        input_fn=train_input_fn,
+        steps=steps)
+    
+    # Define the test inputs
+    test_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+                                    x={"x": test_x},
+                                    y=test_y,
+                                    num_epochs=1,
+                                    shuffle=False)
+    
+    # Evaluate accuracy. 
+    predict_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+      x={"x": test_x},
+      num_epochs=1,
+      shuffle=False)
+
+    y_hat = classifier.predict(input_fn=predict_input_fn)
+    y_hat = list(y_hat)
+    y_hat = np.asarray([int(x['classes'][0]) for x in y_hat]) 
+    test(y_hat, test_y)
+
+
+def mnist_classification():
+    # Load data from file
+    # Make sure that fashion-mnist/*.gz is in data/
+    train_x, train_y, val_x, val_y, test_x, test_y = get_mnist_data(1)
+
+    train_x, val_x, test_x = normalize(train_x, train_x, test_x)    
+
+    train_y = train_y.flatten().astype(np.int32)
+    val_y = val_y.flatten().astype(np.int32)
+    test_y = test_y.flatten().astype(np.int32)
+    num_class = (np.unique(train_y)).shape[0]
+    #pdb.set_trace()
+
+    # DNN parameters
+    hidden_layers = [100, 100, 100]
+    learning_rate = 0.1
+    batch_size = 200
+    steps = 500
+   
+    # Specify that all features have real-value data
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[train_x.shape[1]])]
+
+
+    # Choose activation function
+    activation = tf.nn.relu
+    
+    # Some available gradient descent optimization algorithms 
+    # TODO: [YC1.7] Create optimizer
+    optimizer = tf.train.ProximalAdagradOptimizer(
+                                    learning_rate=learning_rate, 
+                                    l2_regularization_strength=0.001)
+    
+    # build a deep neural network
+    classifier = tf.estimator.DNNClassifier(
+                                    feature_columns=feature_columns,
+                                    hidden_units= hidden_layers,
+                                    n_classes=num_class,
+                                    optimizer= optimizer,
+                                    activation_fn= activation,
+                                    model_dir="problem2-estimator") 
+    
+    # Define the training inputs
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/inputs/numpy_input_fn
+    train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+                                    x={"x": train_x},
+                                    y=train_y,
+                                    num_epochs=None, # None will run forever.
+                                    batch_size = batch_size,
+                                    shuffle=True)
+    
+    # Train model.
+    classifier.train(
+        input_fn=train_input_fn,
+        steps=steps)
+    
+    # Define the test inputs
+    test_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+                                    x={"x": test_x},
+                                    y=test_y,
+                                    num_epochs=1,
+                                    shuffle=False)
+    
+    # Evaluate accuracy. 
+    predict_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+      x={"x": test_x},
+      num_epochs=1,
+      shuffle=False)
+    y_hat = classifier.predict(input_fn=predict_input_fn)
+    y_hat = list(y_hat)
+    y_hat = np.asarray([int(x['classes'][0]) for x in y_hat]) 
+    test(y_hat, test_y)
+
+
+if __name__ == '__main__':
+    np.random.seed(2017) 
+
+    plt.ion()
+    vehicle_classification()
+    #mnist_classification()
